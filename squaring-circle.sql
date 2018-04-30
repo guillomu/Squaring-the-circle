52b65c8d73dda2cd6ee4a813b01c7a33e14b22b3
Guillaume <groult_guigui@hotmail.com>
2018-04-30 12:52:33 +0200

    Modification des requêtes Pas Facile et Ajout du Boss Final

--- squaring-circle.sql
+++ squaring-circle.sql
@@ -1,29 +1,47 @@
 /* Très Facile */
+/* 1. */
 SHOW TABLES FROM webacademie;
 
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'film';
+/* 2. */
+SHOW COLUMNS FROM webacademie.film;
 
-SELECT titre FROM webacademie.film;
+/* 3. */
+SELECT titre AS 'titre film' FROM webacademie.film;
 
+/* 4. */
 SELECT titre FROM webacademie.film WHERE id_genre IS NULL;
 
-SELECT titre FROM webacademie.film WHERE (date_debut_affiche LIKE '2000%') ORDER BY date_debut_affiche DESC LIMIT 10;
+/* 5. */
+SELECT titre FROM webacademie.film WHERE (YEAR(date_debut_affiche) LIKE '2000') ORDER BY date_debut_affiche DESC LIMIT 10;
+
 
 /* Moins facile */
+/* 1. */
 SELECT titre FROM webacademie.film WHERE (id_genre= 1 OR id_genre = 2) AND (titre LIKE 'A%');
 
-SELECT SUM(nbr_siege) AS nombre_siege FROM webacademie.salle;
+/* 2. */
+SELECT SUM(nbr_siege) AS 'nombre siege' FROM webacademie.salle;
 
-SELECT SUM(nbr_siege) AS nombre_siege FROM webacademie.salle GROUP BY etage_salle;
+/* 3. */
+SELECT SUM(nbr_siege) AS 'nombre siege' FROM webacademie.salle GROUP BY etage_salle;
 
+/* 4. */
 SELECT nom_salle FROM webacademie.salle WHERE nbr_siege < 200;
 
-SELECT titre AS titre_cool FROM webacademie.film WHERE (titre LIKE '%day%') AND id_genre = 2;
+/* 5. */
+SELECT titre AS 'titre_cool' FROM webacademie.film WHERE (titre LIKE '%day%') AND id_genre = 2;
+
 
 /* Pas Facile */
-SELECT COUNT(nom) AS nbr_abo, ROUND(AVG(prix)) AS avg_abo FROM webacademie.abonnement;
+/* 1. */
+SELECT COUNT(nom) AS nbr_abo, FLOOR(AVG(prix)) AS 'avg abo' FROM webacademie.abonnement;
 
-SELECT MAX(id_film) AS max_id_film FROM webacademie.film WHERE id_genre = 8 AND date_debut_affiche BETWEEN '1980%' AND '1999%';
+/* 2. */
+SELECT MAX(film.id_film) AS 'max id film' FROM webacademie.film INNER JOIN webacademie.genre ON film.id_genre = genre.id_genre WHERE genre.nom = 'action' AND (YEAR(film.date_debut_affiche) BETWEEN '1980' AND '1999');
 
-SELECT id_membre, date_dernier_film AS last_movie FROM webacademie.membre WHERE id_dernier_film = 2557 OR id_dernier_film = 2741 ORDER BY date_dernier_film DESC;
+/* 3. */
+SELECT DISTINCT membre.id_membre, membre.date_dernier_film AS 'last movie' FROM webacademie.membre INNER JOIN webacademie.historique_membre ON historique_membre.id_membre = membre.id_membre WHERE id_film IN (2557, 2741) ORDER BY membre.date_dernier_film DESC;
 
+
+/* Boss Final */
+SELECT genre.id_genre, IFNULL(genre.nom, '') AS 'nom genre', COUNT(*) AS 'nombre films' FROM webacademie.film INNER JOIN webacademie.genre ON film.id_genre = genre.id_genre WHERE film.id_genre BETWEEN 1 AND 3 GROUP BY film.id_genre;
